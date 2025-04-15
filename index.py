95a428f9cbab071e83547ab1728b24cefed54e90
박성훈 / Koder <koder0205@gmail.com>
2025-04-16 04:27:47 +0900

    feat: calculate tier by avg

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import time
 def prettify(data):
     print(json.dumps(data, indent=4))
 
@@ -12,9 +12,33 @@ def get_all_tags():
 def get_tag_info(taglist):
     ret = []
     for cur in taglist:
-        ret.append({'id': cur['key']})
+        ret.append({'id': cur['key'], 'tier': get_avg_tier(cur['key'])})
+        print(ret[-1])
+        time.sleep(1)
     return ret
+
+def get_avg_tier(tag):
+    url = "https://solved.ac/api/v3/search/problem"
+    params = {
+        'query': f'#{tag}',
+        'sort': 'solved',
+        'direction': 'desc',
+        'page': 1,
+    }
+    res = requests.get(url, params=params)
+
+    sum_tier = 0
+    sum_solved = 0
+    for i in res.json()['items']:
+        sum_tier += i['level'] * i['acceptedUserCount']
+        sum_solved += i['acceptedUserCount']
+
+    return sum_tier // sum_solved 
 
 A = get_all_tags()
 B = get_tag_info(A)
 prettify(B)
+# get_avg_tier("hld")
+
+with open('output.json', 'w') as outfile:
+    json.dump(B, outfile, indent=4)
